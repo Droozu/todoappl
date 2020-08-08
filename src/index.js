39d3b3c5b0671153801b5827783fc5ed43bb40d7
droopy <kozel.nikita@gmail.com>
2020-08-08 12:06:43 +0300

    testlint
    testlint

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 import TodoApp from './components/TodoApp/TodoApp';
-
+//test
 ReactDom.render(<TodoApp />, document.getElementById('root'));
